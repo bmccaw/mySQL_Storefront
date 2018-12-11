44c625c76da37d851761691b93515b4881c659e3
Green Man <brendan.mccaughey@gmail.com>
2018-12-10 22:26:12 -0500

    Got CLI table working with database object.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var Table = require('cli-table');
+
 
 
 //connect to the sql database
@@ -14,13 +16,44 @@ var connection = mysql.createConnection({
     database: 'bamazon'
 });
 
+//connect to the sql database
 connection.connect(function(err) {
     if (err) throw err;
-    init();
-  });
+});
 
-//inital function upon executing the script
-function init() {
+showInventory();
+
+function showInventory () {
+    var productTable = new Table({
+        chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+         , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+         , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+         , 'right': '║' , 'right-mid': '╢' , 'middle': '│' },
+        head: ['item_ID', 'product_name', 'department_name', 'price', 'stock_quantity']
+    });
+    connection.query("SELECT * FROM products", function (err, result) {
+        if (err) throw err;
+        console.log('Current Inventory');
+        console.log('===================\n');
+        
+        for (var i = 0; i<result.length; i++) {
+            var id = result[i].item_id;
+            var name = result[i].product_name;
+            var department = result[i].department_name;
+            var price = result[i].price;
+            var quant = result[i].stock_quantity;
+            productTable.push(
+                [id, name, department, price, quant]
+            );
+        }
+
+        console.log(productTable.toString());
+        promptUser();
+      });
+};
+
+
+function promptUser() {
     inquirer
         .prompt([
             {
@@ -36,7 +69,17 @@ function init() {
         ])
         .then(function(answer) {
             connection.query(
-                'UPDATE products '
+                
+                'UPDATE products SET Select ? WHERE item_id = ?',
+                {
+                    item_id: answer.id,
+                    stock_quantity: answer.quantity,
+                },
+                function(err) {
+                    if (err) throw err;
+                    console.log('Transaction complete.');
+                    init();
+                }
             )
         })
 }
